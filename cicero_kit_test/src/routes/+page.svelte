f474e7b8a197caf9440519a0fee92e0ef5f453c6
jstEagle <hunekejustus@gmail.com>
2024-08-20 18:31:37 +1200

    progress

--- cicero_kit_test/src/routes/+page.svelte
+++ cicero_kit_test/src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script>
     import '$lib/global.css';
 </script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/ComplexExponents">ComplexExponents</a>
-<a href="/ComplexNumbersA2">ComplexNumbersA2</a>
-<a href="/ComplexNumbersintro">ComplexNumbersintro</a>
-<a href="/ComplexPlane">ComplexPlane</a>
-<a href="/DivisionofComplexNumbers">DivisionofComplexNumbers</a>
-<a href="/Polarform">Polarform</a>
+<div class="container">
+    <a href="/ComplexExponents">ComplexExponents</a>
+    <a href="/ComplexNumbersA2">ComplexNumbersA2</a>
+    <a href="/ComplexNumbersintro">ComplexNumbersintro</a>
+    <a href="/ComplexPlane">ComplexPlane</a>
+    <a href="/DivisionofComplexNumbers">DivisionofComplexNumbers</a>
+    <a href="/Polarform">Polarform</a>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
